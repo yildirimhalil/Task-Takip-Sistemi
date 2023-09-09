52f393fff3129edc6936070829fb2d204e1c4a73
Halil <hllyldrm2002.hylrm@gmail.com>
2023-09-09 22:52:59 +0300

    mvc patterni kuruldu

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TaskTakipSistemi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
